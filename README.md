873deb60675d48a56efb0d95282b29b71e261051
mkanenobu <kaneda.1018@gmail.com>
2022-02-28 19:48:13 +0900

    Fix deploy

---
+++ README.md
@@ -0,0 +1,1 @@
+https://mkanenobu.github.io/color-contrast-tool/
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  base:
+    process.env.NODE_ENV === "production" &&
+    process.env.DEPLOY_ENV === "gh-pages"
+      ? "/color-contrast-tool/"
+      : "/",
+});
